4ccee17cce09d97be82297428b27702faec1a881
nbdbkv <nurbakyt.abdibekov@gmail.com>
2021-04-09 15:54:14 +0600

    Add new 'forms' file and register 'CustomUserAdmin' in 'admin'

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+CustomUser = get_user_model()
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        (_('Personal info'), {
+            'fields': ('first_name', 'last_name')
+        }),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups',
+                       'user_permissions')
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2'),
+        }),
+    )
+    list_display = (
+        'phone_number', 'first_name', 'last_name', 'is_staff'
+    )
+    search_fields = ('phone_number', 'first_name', 'last_name')
+    ordering = ('phone_number',)
